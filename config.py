b10ec833c9332f9ab15bae3ee5c23cbd634d7d39
ɴᴏᴏʙ/ᴋᴀʀᴀɴ <132650820+Sidhusaab0001@users.noreply.github.com>
2024-01-25 17:48:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "27827680"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "67ca6eaadbf044a05c06f6840c874cb5"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "6943811163"
+SUPPORT_GRP = "II_CHAT_HUB_II"
+UPDATE_CHNL = "Stylish_Name_Bio"
+OWNER_USERNAME = "imm_Sidhu"
 
